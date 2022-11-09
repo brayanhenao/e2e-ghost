7dc2652886e97b6dd03efbca57158f468ad592ba
Brayan Henao <b.henaoc@uniandes.edu.co>
2022-11-09 13:19:28 -0500

    Add environment setup files

---
+++ kraken/features/feature1-e1.feature
@@ -0,0 +1,7 @@
+Feature: Play game kahoot
+
+  @user1 @web
+  Scenario: My scenario 2
+    Given I go to posts
+    When I click on the first post
+    Then I should see the post
--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -1,110 +1,25 @@
-const {When, Then, BeforeStep} = require('@cucumber/cucumber');
+const {When, Then, BeforeStep, Given} = require('@cucumber/cucumber');
 const expect = require('chai').expect;
+const properties = require('../../../properties.json');
 
-Then('I see that the post is not liked', async function () {
-	let elements = await this.driver.$$(
-		"span[aria-label='See who reacted to this']"
-	);
-	expect(elements.length).to.equal(0);
-});
-
-When('I enter email {kraken-string}', async function (email) {
-	let element = await this.driver.$('#ember6');
-	return element.setValue(email);
-});
-
-When('I enter password {kraken-string}', async function (password) {
-	let element = await this.driver.$('#ember8');
-	return element.setValue(password);
-});
-
-When('I click login', async function () {
-	let element = await this.driver.$('button[type="submit"]');
-	return element.click();
-});
-
-BeforeStep({}, async function () {
+BeforeStep({}, async function() {
+	this.driver.url(`http://localhost:${properties.GHOST_PORT}/ghost/#/signin`);
 	let emailElement = await this.driver.$('#ember6');
-	await emailElement.setValue(email);
-	let passwordElement = await this.driver.$('#ember6');
-	await passwordElement.setValue(email);
+	await emailElement.setValue(properties.EMAIL);
+	await new Promise(r => setTimeout(r, 1000));
+
+	let passwordElement = await this.driver.$('#ember8');
+	await passwordElement.setValue(properties.PASSWORD);
+	await new Promise(r => setTimeout(r, 1000));
+
+	this.driver.takeScreenshot();
+
 	let submitElement = await this.driver.$('button[type="submit"]');
-	return submitElement.click();
+	await submitElement.click();
+
+	return new Promise(r => setTimeout(r, 3000));
 });
 
-When('I go to test', async function () {
-	let element = await this.driver.$(
-		'div[data-functional-selector="my-kahoot-module__662a267d-c95a-4662-8d4a-12a28b9ba169__kahoot-card_title"]'
-	);
-	return element.click();
+Given('I go to posts', async function() {
+	await this.driver.url('http://localhost:2368/posts');
 });
-
-When('I click play on test', async function () {
-	let element = await this.driver.$(
-		'button[data-functional-selector="play-button"]'
-	);
-	return element.click();
-});
-
-When('I select teach mode', async function () {
-	let element = await this.driver.$(
-		'button[data-functional-selector="play-kahoot-dialog__host-live-game"]'
-	);
-	return element.click();
-});
-
-When('I select classic mode', async function () {
-	await this.driver.switchWindow(/play/);
-	let element = await this.driver.$(
-		'button[data-functional-selector="launch-button"]'
-	);
-	return element.click();
-});
-
-When('I send game code to user {int}', async function (userId) {
-	let codeParts = await this.driver.$$(
-		'div[data-functional-selector="game-pin"] > div > div'
-	);
-	let codeStart = await codeParts[0].getText();
-	let codeEnd = await codeParts[1].getText();
-	let code = `${codeStart}${codeEnd}`;
-	await this.driver.writeSignal(userId, code);
-});
-
-When('I click start', async function () {
-	let element = await this.driver.$(
-		'button[data-functional-selector="start-button"]'
-	);
-	return element.click();
-});
-
-When('I click next', async function () {
-	let element = await this.driver.$(
-		'button[data-functional-selector="next-button"]'
-	);
-	return element.click();
-});
-
-/* 
-And I enter email "<EMAIL>"
-        And I wait for 2 seconds
-        And I enter password "<PASSWORD>"
-        And I wait for 2 seconds
-        And I click login
-        And I wait
-        And I go to test
-        And I wait
-        And I click play on test
-        And I select teach mode
-        And I wait
-        And I select classic mode
-        And I wait
-        And I send game code to user 1
-        And I wait
-        And I wait for a signal containing "ready_to_start" for 120 seconds
-        And I click start
-        And I send a signal to user 1 containing "game_started"
-        And I wait for a signal containing "finished" for 120 seconds
-        And I click next
-
-*/
