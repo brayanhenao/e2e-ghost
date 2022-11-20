e781e84deda257507a9ed8d60b79d443c38995b6
Rodrigo Escobar <ocralo@gmail.com>
2022-11-20 18:14:42 -0500

    feat: create fucntions to generate report

--- regresion-visual/resemble/reporter.ts
+++ regresion-visual/resemble/reporter.ts
@@ -1,5 +1,5 @@
 import compareImages from 'resemblejs/compareImages';
-import {ComparisonOptions, ComparisonResult} from 'resemblejs';
+import { ComparisonOptions, ComparisonResult } from 'resemblejs';
 import * as fs from 'fs';
 
 interface FeatureInfo {
@@ -36,7 +36,105 @@ function compareImagesAsync(
 
 function generateReport(featureImagesResults: FeatureImagesResult[]): void {
 	// HTML report
+	fs.writeFileSync(`./report.html`, createReport(featureImagesResults));
+	fs.copyFileSync('./index.css', `./index.css`);
+
+
 }
+
+function createReport(featureImagesResults: FeatureImagesResult[]): string {
+	const datetime = new Date()
+	return `
+	<html>
+		<head>
+			<title> VRT Report </title>
+			<link href="index.css" type="text/css" rel="stylesheet">
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+		</head>
+		<body>
+			<h1>Report for 
+				 <a href="http://localhost:2368"> gost</a>
+			</h1>
+			<p>Executed: ${datetime.toISOString()}</p>
+			<div id="visualizer">
+				${featureImagesResults.map((FeatureImagesResult) => fatureHtml(FeatureImagesResult))}
+			</div>
+			<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+		</body>
+	</html>`
+}
+
+function fatureHtml(featureImage: FeatureImagesResult) {
+	return `
+	<p>
+		${featureImage.feature}
+	</p>
+	${featureImage.scenarios.map((scenary) => scenaryHtml(scenary, featureImage.feature))}
+	`
+
+}
+
+function scenaryHtml(scenary: ScenarioImageResult, feature: string) {
+	return `
+		<div>
+			<p>${scenary.scenario}</p>
+			<p>${scenary.results.misMatchPercentage}</p>
+			<img src="./images/actual/${feature}/${scenary.image}" />
+			<img src="./images/new/${feature}/${scenary.image}" />
+			<img src="./images/diff/${feature}/${scenary.image}" />
+		</div>
+	`
+
+}
+
+//
+
+/* function browser(b:any, info:any) {
+	return `<div class=" browser" id="test0">
+	<div class=" btitle">
+		<h2>Browser: ${b}</h2>
+		<p>Data: ${JSON.stringify(info)}</p>
+	</div>
+	<div class="imgline">
+	  <div class="imgcontainer">
+		<span class="imgname">Reference</span>
+		<img class="img2" src="before-${b}.png" id="refImage" label="Reference">
+	  </div>
+	  <div class="imgcontainer">
+		<span class="imgname">Test</span>
+		<img class="img2" src="after-${b}.png" id="testImage" label="Test">
+	  </div>
+	</div>
+	<div class="imgline">
+	  <div class="imgcontainer">
+		<span class="imgname">Diff</span>
+		<img class="imgfull" src="./compare-${b}.png" id="diffImage" label="Diff">
+	  </div>
+	</div>
+  </div>`
+}
+
+function createReport(datetime:any, resInfo:any) {
+	return `
+	<html>
+		<head>
+			<title> VRT Report </title>
+			<link href="index.css" type="text/css" rel="stylesheet">
+		</head>
+		<body>
+			<h1>Report for 
+				 <a href="${config.url}"> ${config.url}</a>
+			</h1>
+			<p>Executed: ${datetime}</p>
+			<div id="visualizer">
+				${config.browsers.map((b:any) => browser(b, resInfo[b]))}
+			</div>
+		</body>
+	</html>`
+} */
+
+//
 
 async function generateDiffImages(
 	actualGhostImagesPath: string,
@@ -106,7 +204,7 @@ async function Runner() {
 
 	// Clear diff images content
 	if (fs.existsSync(IMAGES_DIFF_GHOST)) {
-		fs.rmdirSync(IMAGES_DIFF_GHOST, {recursive: true});
+		fs.rmdirSync(IMAGES_DIFF_GHOST, { recursive: true });
 		fs.mkdirSync(IMAGES_DIFF_GHOST);
 	}
 
