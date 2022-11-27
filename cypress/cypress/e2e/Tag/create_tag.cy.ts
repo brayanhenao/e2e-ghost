e4429b03a04c4892e306658c3edca4a11ab27667
AndÃrés Cerquera <a.cerquera@uniandes.edu.co>
2022-11-27 17:04:07 -0500

    Implementado ESC1 - F3 con estrategias (apriori, pseudo y aleatorio)

--- cypress/cypress/e2e/Tag/create_tag.cy.ts
+++ cypress/cypress/e2e/Tag/create_tag.cy.ts
@@ -7,9 +7,9 @@ import {
 } from '../../pages';
 
 import {faker} from '@faker-js/faker';
-faker.seed(666); //set seed to keep data consistent
+import {generateManyInvalidTags, generateManyValidTags} from '../../helpers/mock';
 
-describe.skip('create_tag', () => {
+describe('create_tag', () => {
 	before(cy.clearData);
 
 	beforeEach(() => {
@@ -21,7 +21,128 @@ describe.skip('create_tag', () => {
 		});
 	});
 
-	it('should create a public tag', () => {
+	// ESC1 - F3
+	it('should create a public tag with valid data (a-priori)', () => {
+		cy.fixture('data-pool').then(({tags}) => {
+			const randomTag = Math.floor(Math.random() * (tags.valid.length));
+			const tagName = tags.valid[randomTag].name;
+			const tagColor = tags.valid[randomTag].color;
+			const tagDescription = tags.valid[randomTag].description;
+			tagsPage.load().screenshot();
+			tagsPage.newTagButton().click();
+			cy.wait(1000).screenshot();
+			tagsEditPage.nameInput().type(tagName).screenshot();
+			tagsEditPage
+				.colorInput()
+				.type(tagColor)
+				.screenshot();
+			tagsEditPage.descriptionInput().type(tagDescription).screenshot();
+			tagsEditPage.saveButton().click();
+			cy.wait(1000).screenshot();
+
+			tagsPage.load().screenshot();
+			tagsPage.tagListContainer().contains(tagName).should('be.visible');
+			cy.screenshot();
+		});
+	});
+
+	it('should create a public tag with invalid data - invalid types (a-priori)', () => {
+		cy.fixture('data-pool').then(({tags}) => {
+			const randomTag = Math.floor(Math.random() * (tags.invalid.tagsWithInvalidTypesPerField.length));
+			const tagName = tags.invalid.tagsWithInvalidTypesPerField[randomTag].name?.toString();
+			const tagColor = tags.invalid.tagsWithInvalidTypesPerField[randomTag].color?.toString();
+			const tagDescription = tags.invalid.tagsWithInvalidTypesPerField[randomTag].description?.toString();
+			tagsPage.load().screenshot();
+			tagsPage.newTagButton().click();
+			cy.wait(1000).screenshot();
+			tagsEditPage.nameInput().type(tagName, { parseSpecialCharSequences: false }).screenshot();
+			tagsEditPage
+				.colorInput()
+				.type(tagColor, { parseSpecialCharSequences: false })
+				.screenshot();
+			tagsEditPage.descriptionInput().type(tagDescription, { parseSpecialCharSequences: false }).screenshot();
+			tagsEditPage.saveButton().click();
+			cy.wait(1000).screenshot();
+
+			tagsPage.load().screenshot();
+			tagsPage.tagListContainer().contains(tagName).should('be.visible');
+			cy.screenshot();
+		});
+	});
+
+	it('should create a public tag with valid data (pseudo-aleatorio)', () => {
+		cy.wait(1000).screenshot();
+		const randomTags = generateManyValidTags();
+		const randomTag = Math.floor(Math.random() * (randomTags.length));
+		const tagName = randomTags[randomTag].name;
+		const tagDescription = randomTags[randomTag].description;
+		const tagColor = randomTags[randomTag].color;
+		tagsPage.load().screenshot();
+		tagsPage.newTagButton().click();
+		cy.wait(1000).screenshot();
+		tagsEditPage.nameInput().type(tagName).screenshot();
+		tagsEditPage
+			.colorInput()
+			.type(tagColor)
+			.screenshot();
+		tagsEditPage.descriptionInput().type(tagDescription).screenshot();
+		tagsEditPage.saveButton().click();
+		cy.wait(1000).screenshot();
+
+		tagsPage.load().screenshot();
+		tagsPage.tagListContainer().contains(tagName).should('be.visible');
+		cy.screenshot();
+	});
+
+	it('should create a public tag with invalid data - border cases (pseudo-aleatorio)', () => {
+		cy.wait(1000).screenshot();
+		const randomTags = generateManyInvalidTags().tagsWithBorderCases;
+		const randomTag = Math.floor(Math.random() * (randomTags.length));
+		const tagName = randomTags[randomTag].name?.toString();
+		const tagDescription = randomTags[randomTag].description?.toString();
+		const tagColor = randomTags[randomTag].color?.toString();
+		tagsPage.load().screenshot();
+		tagsPage.newTagButton().click();
+		cy.wait(1000).screenshot();
+		tagsEditPage.nameInput().type(tagName, { parseSpecialCharSequences: false }).screenshot();
+		tagsEditPage
+			.colorInput()
+			.type(tagColor, { parseSpecialCharSequences: false })
+			.screenshot();
+		tagsEditPage.descriptionInput().type(tagDescription, { parseSpecialCharSequences: false }).screenshot();
+		tagsEditPage.saveButton().click();
+		cy.wait(1000).screenshot();
+
+		tagsPage.load().screenshot();
+		tagsPage.tagListContainer().contains(tagName).should('be.visible');
+		cy.screenshot();
+	});
+
+	it('should create a public tag with invalid data - missing keys (pseudo-aleatorio)', () => {
+		cy.wait(1000).screenshot();
+		const randomTags = generateManyInvalidTags().tagsWithMissingKeys;
+		const randomTag = Math.floor(Math.random() * (randomTags.length));
+		const tagName = randomTags[randomTag].name?.toString();
+		const tagDescription = randomTags[randomTag].description?.toString();
+		const tagColor = randomTags[randomTag].color?.toString();
+		tagsPage.load().screenshot();
+		tagsPage.newTagButton().click();
+		cy.wait(1000).screenshot();
+		tagsEditPage.nameInput().type(tagName, { parseSpecialCharSequences: false }).screenshot();
+		tagsEditPage
+			.colorInput()
+			.type(tagColor, { parseSpecialCharSequences: false })
+			.screenshot();
+		tagsEditPage.descriptionInput().type(tagDescription, { parseSpecialCharSequences: false }).screenshot();
+		tagsEditPage.saveButton().click();
+		cy.wait(1000).screenshot();
+
+		tagsPage.load().screenshot();
+		tagsPage.tagListContainer().contains(tagName).should('be.visible');
+		cy.screenshot();
+	});
+
+	it('should create a public tag with valid data (aleatorio)', () => {
 		const tagName = faker.lorem.word();
 		tagsPage.load().screenshot();
 		tagsPage.newTagButton().click();
@@ -72,7 +193,7 @@ describe.skip('create_tag', () => {
 			postsEditPage.createPost(
 				faker.lorem.word(),
 				faker.lorem.paragraph(),
-				true
+				true,
 			);
 			cy.wait(1000);
 		}
