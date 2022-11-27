869ab9c8e1941950856e78f16afed826ae07b6f2
AndÃrés Cerquera <a.cerquera@uniandes.edu.co>
2022-11-27 17:25:06 -0500

    Implementado ESC2 - F3 con estrategias (apriori, pseudo y aleatorio)

--- cypress/cypress/e2e/Tag/create_tag.cy.ts
+++ cypress/cypress/e2e/Tag/create_tag.cy.ts
@@ -55,12 +55,12 @@ describe('create_tag', () => {
 			tagsPage.load().screenshot();
 			tagsPage.newTagButton().click();
 			cy.wait(1000).screenshot();
-			tagsEditPage.nameInput().type(tagName, { parseSpecialCharSequences: false }).screenshot();
+			tagsEditPage.nameInput().type(tagName, {parseSpecialCharSequences: false}).screenshot();
 			tagsEditPage
 				.colorInput()
-				.type(tagColor, { parseSpecialCharSequences: false })
+				.type(tagColor, {parseSpecialCharSequences: false})
 				.screenshot();
-			tagsEditPage.descriptionInput().type(tagDescription, { parseSpecialCharSequences: false }).screenshot();
+			tagsEditPage.descriptionInput().type(tagDescription, {parseSpecialCharSequences: false}).screenshot();
 			tagsEditPage.saveButton().click();
 			cy.wait(1000).screenshot();
 
@@ -104,12 +104,12 @@ describe('create_tag', () => {
 		tagsPage.load().screenshot();
 		tagsPage.newTagButton().click();
 		cy.wait(1000).screenshot();
-		tagsEditPage.nameInput().type(tagName, { parseSpecialCharSequences: false }).screenshot();
+		tagsEditPage.nameInput().type(tagName, {parseSpecialCharSequences: false}).screenshot();
 		tagsEditPage
 			.colorInput()
-			.type(tagColor, { parseSpecialCharSequences: false })
+			.type(tagColor, {parseSpecialCharSequences: false})
 			.screenshot();
-		tagsEditPage.descriptionInput().type(tagDescription, { parseSpecialCharSequences: false }).screenshot();
+		tagsEditPage.descriptionInput().type(tagDescription, {parseSpecialCharSequences: false}).screenshot();
 		tagsEditPage.saveButton().click();
 		cy.wait(1000).screenshot();
 
@@ -128,12 +128,12 @@ describe('create_tag', () => {
 		tagsPage.load().screenshot();
 		tagsPage.newTagButton().click();
 		cy.wait(1000).screenshot();
-		tagsEditPage.nameInput().type(tagName, { parseSpecialCharSequences: false }).screenshot();
+		tagsEditPage.nameInput().type(tagName, {parseSpecialCharSequences: false}).screenshot();
 		tagsEditPage
 			.colorInput()
-			.type(tagColor, { parseSpecialCharSequences: false })
+			.type(tagColor, {parseSpecialCharSequences: false})
 			.screenshot();
-		tagsEditPage.descriptionInput().type(tagDescription, { parseSpecialCharSequences: false }).screenshot();
+		tagsEditPage.descriptionInput().type(tagDescription, {parseSpecialCharSequences: false}).screenshot();
 		tagsEditPage.saveButton().click();
 		cy.wait(1000).screenshot();
 
@@ -161,7 +161,269 @@ describe('create_tag', () => {
 		cy.screenshot();
 	});
 
-	it('should count total posts', () => {
+	// ESC2 - F3
+	it('should count total posts with valid tags data (a-priori)', () => {
+		cy.fixture('data-pool').then(({tags}) => {
+			const randomTag = Math.floor(Math.random() * (tags.valid.length));
+			const tagName = tags.valid[randomTag].name;
+			const tagColor = tags.valid[randomTag].color;
+			const tagDescription = tags.valid[randomTag].description;
+			const totalPosts = faker.datatype.number(5);
+
+			tagsPage.load().screenshot();
+			cy.wait(500);
+			tagsPage.newTagButton().click();
+			cy.wait(1000).screenshot();
+			tagsEditPage.nameInput().type(tagName).screenshot();
+			tagsEditPage
+				.colorInput()
+				.type(tagColor)
+				.screenshot();
+			tagsEditPage.descriptionInput().type(tagDescription).screenshot();
+			tagsEditPage.saveButton().click();
+			cy.wait(1000).screenshot();
+
+			for (let i = 0; i < totalPosts; i++) {
+				postsPage.load().screenshot();
+				cy.wait(500);
+				postsPage.newPostsButton().click();
+
+				cy.wait(1000).screenshot();
+
+				postsEditPage.settingsButton().click();
+				postsEditPage.tagInput().type(`${tagName}{enter}`).screenshot();
+				cy.wait(500);
+				postsEditPage.settingsButton().click();
+
+				postsEditPage.createPost(
+					faker.lorem.word(),
+					faker.lorem.paragraph(),
+					true,
+				);
+				cy.wait(1000);
+			}
+
+			tagsPage.load().screenshot();
+			cy.wait(1000);
+			tagsPage
+				.tagListContainer()
+				.children()
+				.contains(`${totalPosts > 1 ? totalPosts + ' post' : totalPosts + ' posts'}`)
+				.should('be.visible');
+			cy.screenshot();
+		});
+	});
+
+	it('should count total posts with invalid tags data - invalid types (a-priori)', () => {
+		cy.fixture('data-pool').then(({tags}) => {
+			const randomTag = Math.floor(Math.random() * (tags.invalid.tagsWithInvalidTypesPerField.length));
+			const tagName = tags.invalid.tagsWithInvalidTypesPerField[randomTag].name?.toString();
+			const tagColor = tags.invalid.tagsWithInvalidTypesPerField[randomTag].color?.toString();
+			const tagDescription = tags.invalid.tagsWithInvalidTypesPerField[randomTag].description?.toString();
+
+			const totalPosts = faker.datatype.number(5);
+
+			tagsPage.load().screenshot();
+			cy.wait(500);
+			tagsPage.newTagButton().click();
+			cy.wait(1000).screenshot();
+			tagsEditPage.nameInput().type(tagName).screenshot();
+			tagsEditPage
+				.colorInput()
+				.type(tagColor)
+				.screenshot();
+			tagsEditPage.descriptionInput().type(tagDescription).screenshot();
+			tagsEditPage.saveButton().click();
+			cy.wait(1000).screenshot();
+
+			for (let i = 0; i < totalPosts; i++) {
+				postsPage.load().screenshot();
+				cy.wait(500);
+				postsPage.newPostsButton().click();
+
+				cy.wait(1000).screenshot();
+
+				postsEditPage.settingsButton().click();
+				postsEditPage.tagInput().type(`${tagName}{enter}`).screenshot();
+				cy.wait(500);
+				postsEditPage.settingsButton().click();
+
+				postsEditPage.createPost(
+					faker.lorem.word(),
+					faker.lorem.paragraph(),
+					true,
+				);
+				cy.wait(1000);
+			}
+
+			tagsPage.load().screenshot();
+			cy.wait(1000);
+			tagsPage
+				.tagListContainer()
+				.children()
+				.contains(`${totalPosts > 1 ? totalPosts + ' post' : totalPosts + ' posts'}`)
+				.should('be.visible');
+			cy.screenshot();
+		});
+	});
+
+	it('should count total posts with valid tags data (pseudo-aleatorio)', () => {
+		const validTags = generateManyValidTags();
+		const randomTag = Math.floor(Math.random() * (validTags.length));
+		const tagName = validTags[randomTag].name?.toString();
+		const tagColor = validTags[randomTag].color?.toString();
+		const tagDescription = validTags[randomTag].description?.toString();
+
+		const totalPosts = faker.datatype.number(5);
+
+		tagsPage.load().screenshot();
+		cy.wait(500);
+		tagsPage.newTagButton().click();
+		cy.wait(1000).screenshot();
+		tagsEditPage.nameInput().type(tagName, {parseSpecialCharSequences: false}).screenshot();
+		tagsEditPage
+			.colorInput()
+			.type(tagColor, {parseSpecialCharSequences: false})
+			.screenshot();
+		tagsEditPage.descriptionInput().type(tagDescription, {parseSpecialCharSequences: false}).screenshot();
+		tagsEditPage.saveButton().click();
+		cy.wait(1000).screenshot();
+
+		for (let i = 0; i < totalPosts; i++) {
+			postsPage.load().screenshot();
+			cy.wait(500);
+			postsPage.newPostsButton().click();
+
+			cy.wait(1000).screenshot();
+
+			postsEditPage.settingsButton().click();
+			postsEditPage.tagInput().type(`${tagName}{enter}`).screenshot();
+			cy.wait(500);
+			postsEditPage.settingsButton().click();
+
+			postsEditPage.createPost(
+				faker.lorem.word(),
+				faker.lorem.paragraph(),
+				true,
+			);
+			cy.wait(1000);
+		}
+
+		tagsPage.load().screenshot();
+		cy.wait(1000);
+		tagsPage
+			.tagListContainer()
+			.children()
+			.contains(`${totalPosts > 1 ? totalPosts + ' post' : totalPosts + ' posts'}`)
+			.should('be.visible');
+		cy.screenshot();
+	});
+
+	it('should count total posts with invalid tags data - border cases (pseudo-aleatorio)', () => {
+		const tags = generateManyInvalidTags().tagsWithBorderCases;
+		const randomTag = Math.floor(Math.random() * (tags.length));
+		const tagName = tags[randomTag].name?.toString();
+		const tagColor = tags[randomTag].color?.toString();
+		const tagDescription = tags[randomTag].description?.toString();
+
+		const totalPosts = faker.datatype.number(5);
+
+		tagsPage.load().screenshot();
+		cy.wait(500);
+		tagsPage.newTagButton().click();
+		cy.wait(1000).screenshot();
+		tagsEditPage.nameInput().type(tagName, {parseSpecialCharSequences: false}).screenshot();
+		tagsEditPage
+			.colorInput()
+			.type(tagColor, {parseSpecialCharSequences: false})
+			.screenshot();
+		tagsEditPage.descriptionInput().type(tagDescription, {parseSpecialCharSequences: false}).screenshot();
+		tagsEditPage.saveButton().click();
+		cy.wait(1000).screenshot();
+
+		for (let i = 0; i < totalPosts; i++) {
+			postsPage.load().screenshot();
+			cy.wait(500);
+			postsPage.newPostsButton().click();
+
+			cy.wait(1000).screenshot();
+
+			postsEditPage.settingsButton().click();
+			postsEditPage.tagInput().type(`${tagName}{enter}`).screenshot();
+			cy.wait(500);
+			postsEditPage.settingsButton().click();
+
+			postsEditPage.createPost(
+				faker.lorem.word(),
+				faker.lorem.paragraph(),
+				true,
+			);
+			cy.wait(1000);
+		}
+
+		tagsPage.load().screenshot();
+		cy.wait(1000);
+		tagsPage
+			.tagListContainer()
+			.children()
+			.contains(`${totalPosts > 1 ? totalPosts + ' post' : totalPosts + ' posts'}`)
+			.should('be.visible');
+		cy.screenshot();
+	});
+
+	it('should count total posts with invalid tags data - missing keys (pseudo-aleatorio)', () => {
+		const tags = generateManyInvalidTags().tagsWithMissingKeys;
+		const randomTag = Math.floor(Math.random() * (tags.length));
+		const tagName = tags[randomTag].name?.toString();
+		const tagColor = tags[randomTag].color?.toString();
+		const tagDescription = tags[randomTag].description?.toString();
+
+		const totalPosts = faker.datatype.number(5);
+
+		tagsPage.load().screenshot();
+		cy.wait(500);
+		tagsPage.newTagButton().click();
+		cy.wait(1000).screenshot();
+		tagsEditPage.nameInput().type(tagName, {parseSpecialCharSequences: false}).screenshot();
+		tagsEditPage
+			.colorInput()
+			.type(tagColor, {parseSpecialCharSequences: false})
+			.screenshot();
+		tagsEditPage.descriptionInput().type(tagDescription, {parseSpecialCharSequences: false}).screenshot();
+		tagsEditPage.saveButton().click();
+		cy.wait(1000).screenshot();
+
+		for (let i = 0; i < totalPosts; i++) {
+			postsPage.load().screenshot();
+			cy.wait(500);
+			postsPage.newPostsButton().click();
+
+			cy.wait(1000).screenshot();
+
+			postsEditPage.settingsButton().click();
+			postsEditPage.tagInput().type(`${tagName}{enter}`).screenshot();
+			cy.wait(500);
+			postsEditPage.settingsButton().click();
+
+			postsEditPage.createPost(
+				faker.lorem.word(),
+				faker.lorem.paragraph(),
+				true,
+			);
+			cy.wait(1000);
+		}
+
+		tagsPage.load().screenshot();
+		cy.wait(1000);
+		tagsPage
+			.tagListContainer()
+			.children()
+			.contains(`${totalPosts > 1 ? totalPosts + ' post' : totalPosts + ' posts'}`)
+			.should('be.visible');
+		cy.screenshot();
+	});
+
+	it('should count total posts with valid tags data (aleatorio)', () => {
 		const tagName = faker.lorem.word();
 		const totalPosts = faker.datatype.number(5);
 
@@ -203,7 +465,7 @@ describe('create_tag', () => {
 		tagsPage
 			.tagListContainer()
 			.children()
-			.contains(`${totalPosts} posts`)
+			.contains(`${totalPosts > 1 ? totalPosts + ' post' : totalPosts + ' posts'}`)
 			.should('be.visible');
 		cy.screenshot();
 	});
